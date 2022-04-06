378059600f84ba81ebb1a4fbf57e84da16cf7f5b
sahilbansaldevops <102287082+sahilbansaldevops@users.noreply.github.com>
2022-04-06 16:57:02 +0530

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 resource azurerm_resource_group resourceGroup {
     name = var.resourceGroupName
     location = var.resourceGroupLocation
-
-    tags = var.resourceGroupTags
 }
 
 
 variable resourceGroupName { type= string}
 variable resourceGroupLocation { type= string}
-variable resourceGroupTags { type =  map(string)}
+
